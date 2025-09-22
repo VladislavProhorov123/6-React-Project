166afc5ccbe6016a0d68af7a5750877e7457b04f
Vladislav <vladislavprokhorov1005@gmail.com>
2025-09-22 20:14:13 +0300

    quiz game

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
 import { useState } from "react";
+import Game from "./component/Game";
+import Result from "./component/Result";
+import { QUESTIONS } from "./questions.data";
 
 function App() {
-  const [open, setOpen] = useState(false);
+  const [step, setStep] = useState(0);
+  const [correct, setCorrect] = useState(0);
+
+  const question = QUESTIONS[step];
+
+  const onClickVariant = (index) => {
+    console.log(step, index);
+    setStep(step + 1);
+
+    if(index === question.correct) {
+      setCorrect(correct + 1)
+    }
+  };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <button className="cursor-pointer px-6 py-2 rounded-lg bg-indigo-600 text-white hover:bg-indigo-700 transition" onClick={() => setOpen(!open)}>Открыть окно</button>
-      {open && (
-        <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-          <div className="bg-white rounded-2xl shadow-xl w-full max-w-lg p-6 relative">
-            <button className="text-[22px] absolute top-4 right-4 text-gray-500 hover:text-gray-700" onClick={() => setOpen(false)}>
-              <i class="ri-close-line"></i>
-            </button>
-            <div className="flex flex-col items-center mb-6">
-              <img className="w-20 h-20 rounded-full object-cover mb-3 shadow-md" src="/ava2.jpg" alt="" />
-              <h3 className="text-lg font-semibold">Fronted Developer</h3>
-              <p className="text-gray-500 text-sm">Ukraine, Kyiv</p>
-            </div>
-            <h1 className="text-xl font-bold text-gray-800 mb-3 text-center">
-              Why Branches in Git Are Perfect for Managing Different Projects
-            </h1>
-            <p className="text-gray-600 leading-relaxed text-center">
-              Branches in Git are a powerful way to separate different versions
-              of your code. You can keep one project in the main branch and
-              start a completely new one in another branch. This makes it easy
-              to switch between projects, experiment safely, and keep your
-              GitHub repository clean.
-            </p>
-          </div>
-        </div>
+    <div className="">
+      {step !== QUESTIONS.length ? (
+        <Game step={step} question={question} onClickVariant={onClickVariant} />
+      ) : (
+        <Result correct={correct} />
       )}
     </div>
   );
