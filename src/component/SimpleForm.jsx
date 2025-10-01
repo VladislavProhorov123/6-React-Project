6545ec28c99b5203ea9cdd9c71993124be914b98
Vladislav <vladislavprokhorov1005@gmail.com>
2025-10-01 19:15:09 +0300

    add style for react form

--- src/component/SimpleForm.jsx
+++ src/component/SimpleForm.jsx
@@ -25,7 +25,7 @@ export default function SimpleForm() {
   const countries = [
     { code: "+1", name: "USA" },
     { code: "+44", name: "UK" },
-    { code: "+380", name: "Ukraine" },
+    { code: "+380", name: "Ukraine"},
   ];
 
   const selectedCode = watch("countryCode");
@@ -41,13 +41,13 @@ export default function SimpleForm() {
 
   const onSubmit = (data) => console.log(data);
   return (
-    <div className=" px-[15px] py-[20px] mx-auto max-w-[600px] border">
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <div className=" px-[15px] py-[20px] mx-auto max-w-[600px] border rounded-xl shadow-md bg-white">
+      <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
         <div className="flex gap-4">
-          <label className="flex-1 flex flex-col">
+          <label className="flex-1 flex flex-col text-sm font-medium text-gray-700">
             Имя
             <input
-              className="border"
+              className="border border-gray-400  p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 "
               type="text"
               {...register("firstName", {
                 required: "Обязательное поле",
@@ -55,12 +55,16 @@ export default function SimpleForm() {
                 maxLength: { value: 30, message: "Максимум 30 символов" },
               })}
             />
-            {errors.firstName && <p>{errors.firstName.message}</p>}
+            {errors.firstName && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.firstName.message}
+              </p>
+            )}
           </label>
-          <label htmlFor="">
+          <label className="flex-1 flex flex-col text-sm font-medium text-gray-700">
             Фамилия
             <input
-              className="border"
+              className="border border-gray-400  p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               type="text"
               {...register("lastName", {
                 required: "Обязательное поле",
@@ -68,38 +72,48 @@ export default function SimpleForm() {
                 maxLength: { value: 30, message: "Максимум 30 символов" },
               })}
             />
-            {errors.lastName && <p>{errors.lastName.message}</p>}
+            {errors.lastName && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.lastName.message}
+              </p>
+            )}
           </label>
         </div>
 
-        <div className="">
+        <div className="flex">
           <label>
-            
             <select
+              className="border border-gray-400 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 mr-[10px]"
               {...register("countryCode", { required: "Выберите страну" })}
             >
-              <option value="">Выберите страну</option>
+              <option value="" disabled>Выберите страну</option>
               {countries.map((c) => (
                 <option key={c.code} value={c.code}>
-                  {c.name} ({c.code})
+                   {c.name} ({c.code})
                 </option>
               ))}
             </select>
             <input
               type="tel"
-              className="border"
+              className="border border-gray-400  p-2 flex-1 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               {...register("phone", { required: "Введите номер телефона" })}
             />
-            {errors.countryCode && <p>{errors.countryCode.message}</p>}
-            {errors.phone && <p>{errors.phone.message}</p>}
+            {errors.countryCode && (
+              <p className="text-red-500 text-xs">
+                {errors.countryCode.message}
+              </p>
+            )}
+            {errors.phone && (
+              <p className="text-red-500 text-xs">{errors.phone.message}</p>
+            )}
           </label>
         </div>
 
-        <div className="">
-          <label>
+        <div className="flex gap-4">
+          <label className="flex-1 flex flex-col text-sm font-medium text-gray-700">
             Пароль
             <input
-              className="border"
+              className="border border-gray-400  p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               type="password"
               {...register("password", {
                 required: "Введите пароль",
@@ -112,9 +126,16 @@ export default function SimpleForm() {
                 },
               })}
             />
-            {errors.password && <p>{errors.password.message}</p>}
+            {errors.password && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.password.message}
+              </p>
+            )}
+          </label>
+          <label className="flex-1 flex flex-col text-sm font-medium text-gray-700">
+            Подтверждение
             <input
-              className="border"
+              className="border border-gray-400  p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               type="password"
               {...register("confirmPassword", {
                 required: "Подтвердите пароль",
@@ -122,14 +143,15 @@ export default function SimpleForm() {
                   value === watch("password") || "Пароли не совпадают",
               })}
             />
-            {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
+            {errors.confirmPassword && <p className="text-red-500 text-xs mt-1">{errors.confirmPassword.message}</p>}
           </label>
         </div>
-        <label>
+
+        <label className="flex flex-col text-sm font-medium text-gray-700">
           Email
           <input
             placeholder="Введите email"
-            className="border"
+            className="border border-gray-400  p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             {...register("email", {
               required: "Введите email",
               maxLength: { value: 50, message: "Максимум 50 символов" },
@@ -139,22 +161,22 @@ export default function SimpleForm() {
               },
             })}
           />
-          {errors.email && <p>{errors.email.message}</p>}
+          {errors.email && <p className="text-red-500 text-xs mt-1">{errors.email.message}</p>}
         </label>
-        <label>
+        <label className="flex flex-col text-sm font-medium text-gray-700">
           Сообщение
           <textarea
-            className="border"
+            className="border border-gray-400  p-2 rounded-md h-24 resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
             {...register("message", {
               required: "Введите сообщение",
               maxLength: { value: 500, message: "Максимум 500 символов" },
               minLength: { value: 10, message: "Минимум 10 символов" },
             })}
           ></textarea>
-          {errors.message && <p>{errors.message.message}</p>}
+          {errors.message && <p className="text-red-500 text-xs mt-1">{errors.message.message}</p>}
         </label>
 
-        <button type="submit">Отправить</button>
+        <button className="w-full bg-blue-600 text-white font-semibold rounded-md py-3 hover:bg-blue-700 transition cursor-pointer" type="submit">Отправить</button>
       </form>
     </div>
   );
